d97576de6329f3e086a31233ee5893050625fdfe
Drakomire <65521138+Drakomire@users.noreply.github.com>
2020-08-03 21:46:28 -0400

    Added first files

---
+++ cmake-build-debug/CMakeFiles/snowman.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snowman.dir/main.c.obj"
+  "libsnowman.dll.a"
+  "snowman.exe"
+  "snowman.exe.manifest"
+  "snowman.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/snowman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,9 @@
+//
+// Created by Drakomire on 8/3/2020.
+//
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello");
+}
