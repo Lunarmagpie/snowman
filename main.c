d868573c2e6e130c58a7ae9d069b0d5610631b14
Drakomire <65521138+Drakomire@users.noreply.github.com>
2020-08-05 19:13:11 -0400

    Update main.c

--- main.c
+++ main.c
@@ -8,23 +8,23 @@
 
 int main(){
     while (1){
-        int livesUsed = 0;
-        int guessArr[26] = {0};
-        char word[100] = "hello";
+        int livesUsed = 0; //The amount of failed guesses
+        int guessArr[26] = {0}; //Array of characters that where guessed
+        char word[100] = "hello"; //The word that the player has to guess
 
         //This is the main gameplay loop
         while (1) {
             makeGuess(guessArr, &livesUsed, word);
-            if (drawSnowman(guessArr, livesUsed, word)){ //This is how to call the function. give it the guesses, number of guesses, and the correct string
+            if (drawSnowman(guessArr, livesUsed, word)){
+                //This code executes when the player wins
                 printf("You guessed the word!");
                 break;
-            }
-            if (livesUsed>10){
+            }else{
+                //This code executes when the player looses
+                printf("You lost the game. The word was %s.", word);
                 break;
             }
         }
-        //This code executes when the player looses
-        printf("You lost the game. The word was %s.", word);
 
         //End the game. Get rid of this so the player can retry.
         break;
